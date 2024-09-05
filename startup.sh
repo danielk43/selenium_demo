5384f2ad996ea53895bb0f3992bff9a859b93b55
danielk43 <11856117+danielk43@users.noreply.github.com>
2024-09-05 05:41:20 -0400

    Add short sleep after x11 init

--- startup.sh
+++ startup.sh
@@ -8,8 +8,10 @@ Xvfb :99 -screen 0 1920x1080x16 &
 # Run x11vnc on display 99
 x11vnc -display :99 -forever -ncache 10 -usepw &
 
+sleep 1
+
 # Compile and run NUnit tests
 # Or pass non-default cmds as args
 chown -R sdk_user: /data
-echo "CMD: ${@}"
+echo "CMD=${@}"
 runuser -u sdk_user -- "${@}"
